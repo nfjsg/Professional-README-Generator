2cf8a73720a5ccfb7a8404befa8acc46956053cc
nfjsg <140765984+nfjsg@users.noreply.github.com>
2023-09-29 20:05:04 -0400

    Update generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,40 +1,66 @@
 function renderLicenseBadge(license) {
   if (license) {
-    // You can generate and return the appropriate badge URL here
-    // Example for a license badge URL from shields.io:
-    // return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
   }
   return '';
 }
+
 function renderLicenseLink(license) {
   if (license) {
-    // You can return a link to the license here
-    // Example link for the MIT License:
-    // return `[License](https://opensource.org/licenses/MIT)`;
+    return `[License](https://opensource.org/licenses/${license})`;
   }
   return '';
 }
+
 function renderLicenseSection(license) {
   if (license) {
-    // You can return the license section content here
-    // Example for the MIT License:
-    // return `## License
-
-    // This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).`;
+    return `This project is licensed under the [${license} License](${renderLicenseLink(
+      license
+    )}).`;
   }
   return '';
 }
+
 function generateMarkdown(data) {
   const licenseBadge = renderLicenseBadge(data.license);
-  const licenseLink = renderLicenseLink(data.license);
   const licenseSection = renderLicenseSection(data.license);
 
   return `# ${data.title}
 
 ${licenseBadge}
 
-...
+## Description
+${data.description}
 
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
 ${licenseSection}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions or need further assistance, please feel free to reach out to me:
+
+- GitHub: [${data.username}](https://github.com/${data.username})
+- Email: ${data.email}
 `;
 }
+
+module.exports = generateMarkdown;
